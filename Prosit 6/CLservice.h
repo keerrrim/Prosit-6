fa0301c7f8f26fe4faeabe69115034521bef8d32
33761 <33761@KARIMHP>
2023-11-29 13:07:37 +0100

    Ajoutez des fichiers projet.

---
+++ Prosit 6/CLservice.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "CLmapTB.h"
+#include "cad.h"
+
+namespace NS_Comp_Svc
+{
+	ref class CLservices
+	{
+	private:
+		NS_Comp_Data::CLcad^ oCad;
+		NS_Comp_Mappage::CLmapTB^ oMappTB;
+	public:
+		CLservices(void);
+		System::Data::DataSet^ selectionnerToutesLesPersonnes(System::String^);
+		void ajouterUnePersonne(System::String^, System::String^);
+	};
+}
+
+ref class CLservice
+{
+};
+
